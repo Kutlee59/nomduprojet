787246c51f95948d796ca242e6330ab3d74de65a
Kutlee59 <tafroukte.t@gmail.com>
2023-01-23 09:59:26 +0100

    tfk

---
+++ src/components/hearder/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './header.css'
+
+function Header() {
+  return (
+    <>
+    <h1>Header</h1>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+    </>
+  )
+}
+
+export default Header
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/hearder/Header'
+
+function Home() {
+  return (
+    <>
+    <Header />
+
+    <h1>Accueil</h1>
+    </>
+  )
+}
+
+export default Home
